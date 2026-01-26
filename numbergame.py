e26eed356d140e36f0dea43df6303bfe694efa4d
Portia <portiamasetla20@gmail.com>
2026-01-26 19:46:41 +0200

    Updated the file

--- numbergame.py
+++ numbergame.py
@@ -2,20 +2,20 @@ import random
 
 def guess_game():
     # Generates a random integer
-    secret_num = random.randint(1, 200)
+    secret_num = random.randint(1, 100)
     # print(secret_num)
-
+    print("Guess a number between 1-100")
     while True:
-        user_input = int(input("Enter your guess: "))
+        user_input = int(input("\nEnter your guess: "))
 
         if secret_num > user_input:
-            print("Guess a higher number") # guess a higher number
+            print("\nGuess a higher number") # guess a higher number
 
         elif secret_num < user_input:
-            print("Guess a lower number") # guess a lower number
+            print("\nGuess a lower number") # guess a lower number
 
         elif user_input == secret_num:
-            print("Correct!")  
+            print("\nCorrect!")  
             print(f"Guess is {secret_num}")
             break
 
