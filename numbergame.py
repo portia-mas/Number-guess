50ed70eaabc1b2d6bd943c41977b6c830d96e9c9
Portia <portiamasetla20@gmail.com>
2026-01-26 19:33:25 +0200

    feat:added more features

--- numbergame.py
+++ numbergame.py
@@ -3,13 +3,13 @@ import random
 def guess_game():
     # Generates a random integer
     secret_num = random.randint(1, 200)
-    print(secret_num)
+    # print(secret_num)
 
     while True:
         user_input = int(input("Enter your guess: "))
 
         if secret_num > user_input:
-            print("Higher") # guess a higher number
+            print("Guess a higher number") # guess a higher number
 
         elif secret_num < user_input:
             print("Lower")
